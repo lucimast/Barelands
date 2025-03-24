2763f272d38a9e6d10d55a0655da31fd55d9bce8
lucimast <lucimast@gmail.com>
2025-03-24 15:38:42 +0100

    Update: Changes made through admin panel and API updates

---
+++ build/static/chunks/src_app_layout_tsx_f0e4c1a2._.js
@@ -0,0 +1,19 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_layout_tsx_f0e4c1a2._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_layout_tsx_f0e4c1a2._.js",
+  "chunks": [
+    "static/chunks/_fbeb6a8e._.css",
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_a4c796c4._.js",
+    "static/chunks/node_modules_next_dist_489785d6._.js",
+    "static/chunks/node_modules_react-icons_io5_index_mjs_39106f7b._.js",
+    "static/chunks/node_modules_react-icons_fi_index_mjs_9cbf4bb1._.js",
+    "static/chunks/node_modules_react-icons_lib_74ccc930._.js",
+    "static/chunks/node_modules_framer-motion_dist_es_8669c626._.js",
+    "static/chunks/node_modules_4f04997c._.js",
+    "static/chunks/src_cfaad742._.js"
+  ],
+  "source": "dynamic"
+});
